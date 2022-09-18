2c8f49e6905955a49dd84483ccc0cb31281ddd29
Tsm8542 <shaikhabid932@gmail.com>
2022-09-19 00:26:13 +0530

    19.09.2022 12:26AM

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-function x(){
-    var x = document.getElementById("text-input").value
-    document.querySelector(".preview").innerHTML=x
+function s(){
+    var x = document.getElementById("tt").value;
+    var file = new Blob([x],{type:"text"});
+    var a = document.createElement("a");
+    a.href = URL.createObjectURL(file);
+    a.download = "Project.txt";
+    a.click()
 }
 
+function l(){
+    var file = document.getElementById("load").files[0];
+    var reader = new FileReader();
+    reader.onloadend = function(){
+        var load = (reader.result);
+        document.getElementById("tt").value = load;
+    }
+    reader.readAsText(file);
+}
 function p(){
-    window.print()
+    window.print();
 }
